41e7d2ae990f66613600b597eac5fe3a7aa07ff4
Bruno <brunoapimentel@gmail.com>
2017-10-18 21:27:21 -0200

    Aplica funcionalidade ao componente form

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
-export class FormComponent implements OnInit {
+export class FormComponent {
+  tarefas = [];
 
-  constructor() { }
+  constructor() {
+  }
 
-  ngOnInit() {
+  inserir(input: HTMLInputElement){
+    this.tarefas.push({
+      titulo: input.value,
+      feita: false
+    });
+
+    input.value = '';
+    console.log(this.tarefas);
   }
 
 }
